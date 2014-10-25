7c027ac194cd04ca545abc0646c2cc61a583e174
Ben Liang <bliang@choicestream.com>
2014-10-25 02:35:05 -0400

    NF: new view function for index & changed settings to include bootstrap as installed app

--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 import requests
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print r.text
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    return render(request, 'index.html', {})
 
 def db(request):
 
