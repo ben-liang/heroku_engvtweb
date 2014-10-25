b8023cc49511971dd6107d7dd5cc4a68da79596b
Ben Liang <bliang@choicestream.com>
2014-10-25 02:04:56 -0400

    RF: changed getting started to engvtweb

--- engvtweb/settings.py
+++ engvtweb/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for gettingstarted project.
+Django settings for project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
@@ -51,9 +51,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'gettingstarted.urls'
+ROOT_URLCONF = 'engvtweb.urls'
 
-WSGI_APPLICATION = 'gettingstarted.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
