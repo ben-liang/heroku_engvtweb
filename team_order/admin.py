32dc60f7aedb863c5b7d1714011b321c398d1fe4
Ben Liang <bliang@choicestream.com>
2014-10-26 02:31:33 -0400

    NF: Admin for team order

--- team_order/admin.py
+++ team_order/admin.py
@@ -1,1 +1,22 @@
 __author__ = 'bliang'
+from django.contrib import admin
+from .models import *
+
+class QBPPartsAdmin(admin.ModelAdmin):
+    list_display = ['prodid', 'category', 'brand__brand', 'model_description',
+                    'size','color','msrp','each_cost','product_description']
+    date_hierarchy = 'tstamp'
+    readonly_fields = ['prodid', 'category', 'brand__brand', 'model_description',
+                    'size','color','msrp','each_cost','product_description']
+    search_fields = ['prodid', 'category', 'brand__brand', 'model_description',
+                    'size','color','msrp','each_cost','product_description']
+    list_filter = ['category','brand__brand','size']
+    list_select_related = True
+
+class QBPBrandsAdmin(admin.ModelAdmin):
+    list_display = ['brand']
+    search_fields = ['brand']
+    readonly_fields = ['brand']
+
+admin.site.register(QbpPart,QBPPartsAdmin)
+admin.site.register(QbpBrand,QBPBrandsAdmin)
