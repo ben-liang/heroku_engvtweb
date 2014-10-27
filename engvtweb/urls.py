125bbfecf565859e230b480267b089a4c7528015
Ben Liang <bliang@choicestream.com>
2014-10-26 22:24:24 -0400

    RF: Moved index view & template to engvtweb

--- engvtweb/urls.py
+++ engvtweb/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
 admin.autodiscover()
 
+import views
 import hello.views
 
 urlpatterns = patterns('',
@@ -10,8 +11,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'gettingstarted.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', views.index, name='index'),
+    # url(r'^db', hello.views.db, name='db'),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
