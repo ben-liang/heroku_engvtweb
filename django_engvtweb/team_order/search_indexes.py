21c261c20f68afb53159ee82a4586bd7982333a7
Ben Liang <bliang@choicestream.com>
2014-10-28 22:32:54 -0400

    NF: Change search fields brand and category to faceted, which will allow for faceted search

--- django_engvtweb/team_order/search_indexes.py
+++ django_engvtweb/team_order/search_indexes.py
@@ -5,9 +5,9 @@ from models import *
 class QbpPartIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     product_description = indexes.CharField(model_attr='product_description')
-    brand = indexes.CharField(model_attr='brand')
+    brand = indexes.CharField(model_attr='brand', faceted=True)
     prodid = indexes.CharField(model_attr='prodid')
-    category = indexes.CharField(model_attr='category')
+    category = indexes.CharField(model_attr='category', faceted=True)
     model = indexes.CharField(model_attr='model')
 
     def get_model(self):
