17681fbe16a39789540ba9953e4f0db27e2b9666
Ben Liang <bliang@choicestream.com>
2014-10-31 19:34:58 -0400

    BF: Made min value of add to cart form 1

--- django_engvtweb/cart/forms.py
+++ django_engvtweb/cart/forms.py
@@ -7,19 +7,20 @@ class AddToCartForm(forms.Form):
     #be easier to keep as CharField so that choices don't have to be modified
     #as new objects are added
     object_type = forms.CharField(widget=forms.HiddenInput)
-    quantity = forms.IntegerField()
+    quantity = forms.IntegerField(min_value=1)
 
     def clean(self):
         cleaned_data = super(AddToCartForm, self).clean()
         return cleaned_data
 
-class RemoveFromCartForm(forms.Form):
+class UpdateQuantityForm(forms.Form):
     object_id = forms.IntegerField(widget=forms.HiddenInput)
     #may want to make this a choices field but it might just
     #be easier to keep as CharField so that choices don't have to be modified
     #as new objects are added
     object_type = forms.CharField(widget=forms.HiddenInput)
 
+
     def clean(self):
-        cleaned_data = super(RemoveFromCartForm, self).clean()
+        cleaned_data = super(UpdateQuantityForm, self).clean()
         return cleaned_data
