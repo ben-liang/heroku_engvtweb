f471dc3ef19ddb9bf09ca81c8ba83808de869e93
Ben Liang <bliang@cliquesads.com>
2016-03-18 23:45:36 -0600

    BF: Update management command for new argparsing method

--- django_engvtweb/team_order/management/commands/import_new_qbp_catalog.py
+++ django_engvtweb/team_order/management/commands/import_new_qbp_catalog.py
@@ -1,15 +1,16 @@
 from django.core.management.base import BaseCommand
-from optparse import make_option
 from django_engvtweb.team_order.utils import import_new_qbpcatalog, DEFAULT_PARTS_FILE
 
+
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-        make_option('--file',
-            action='store',
-            dest='file',
-            default=False,
-            help='Full path to qbpcatalog.txt file')
-        )
+
+    def add_arguments(self, parser):
+        parser.add_argument('--file',
+                            action='store',
+                            dest='file',
+                            default=False,
+                            help='Full path to qbpcatalog.txt file')
+
     def handle(self, *args, **options):
         if options['file']:
             file = options['file']
