b58a5bf32abaf50623e82b5251375e31917c1d51
Ben Liang <bliang@cliquesads.com>
2016-03-19 18:23:29 -0600

    Just remove args altogether

--- django_engvtweb/team_order/management/commands/import_new_qbp_catalog.py
+++ django_engvtweb/team_order/management/commands/import_new_qbp_catalog.py
@@ -4,18 +4,8 @@ from django_engvtweb.team_order.utils import import_new_qbpcatalog, DEFAULT_PART
 
 class Command(BaseCommand):
 
-    def add_arguments(self, parser):
-        parser.add_argument('--filepath',
-                            action='store_true',
-                            dest='filepath',
-                            default=False,
-                            help='Full path to qbpcatalog.txt file')
-
     def handle(self, *args, **options):
-        if options['filepath']:
-            filepath = options['filepath']
-        else:
-            filepath = DEFAULT_PARTS_FILE
+        filepath = DEFAULT_PARTS_FILE
         self.stdout.write('== Beginning import of QBP Catalog using file: %s ==' % filepath)
         import_new_qbpcatalog(filepath)
         self.stdout.write('== Done ==')
