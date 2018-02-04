80dc3ed61cbf189637790aeb2fab0919b4ee9e3d
Ben Liang <bliang@cliquesads.com>
2018-02-04 01:32:56 -0500

    Change logo & revise URLs & navbar

--- django_engvtweb/team_order/urls.py
+++ django_engvtweb/team_order/urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns('',
     url(r'^qbp/', QBPSearchView(form_class=QbpForm,
                                 template='team_order/qbp.html',
                                 searchqueryset=qbp_sqs), name='qbp'),
-    url(r'^bike/', BikeList.as_view(), name='bike'),
-    url(r'^stages/', OtherPartList.as_view(brand_name='Stages',title='Stages Powermeters'), name='stages'),
+    # url(r'^bike/', BikeList.as_view(), name='bike'),
+    url(r'^schwalbe/', OtherPartList.as_view(brand_name='Schwalbe',title='Schwalbe Tires'), name='schwalbe'),
     url(r'^team-order-details/', TeamOrderDetailsView.as_view(), name='team-order-details'),
     url(r'^send-order-details/', handle_email_form, name='send-order-details')
 )
